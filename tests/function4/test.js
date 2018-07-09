37500e497e246ad99848c5534c49f1815bc735d3
Shanmuga <notifyshan@gmail.com>
2018-07-09 14:58:49 +1000

    fun4 added

---
+++ tests/function4/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var test = require('unit.js');
+var function4 = require('../../functions/function4.js');
+
+describe('Tests function3', function() {
+  it('verifies successful response', function(done) {
+    function4.handler({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.string(result.body).contains('This is function4 Lambda Demo');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
