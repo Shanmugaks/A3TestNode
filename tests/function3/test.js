67e7f6c39968b06117b76008b4d0e6ac4b45501c
Shanmuga <notifyshan@gmail.com>
2018-07-09 11:58:30 +1000

    3rd function

---
+++ tests/function3/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var test = require('unit.js');
+var function3 = require('../../functions/function3.js');
+
+describe('Tests function3', function() {
+  it('verifies successful response', function(done) {
+    function3.handler({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.string(result.body).contains('This is function3 Lambda Demo');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
