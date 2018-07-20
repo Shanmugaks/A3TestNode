076e2b4f78e4eb0a91b020aa3e6a9cface44f978
Shanmuga <notifyshan@gmail.com>
2018-07-20 13:24:43 +1000

    dynamodb updated for SAM template

--- functions/supercommon/dynamodb.js
+++ functions/supercommon/dynamodb.js
@@ -13,7 +13,7 @@
 
 import AWS from 'aws-sdk';
 
-AWS.config.update({ region: process.env.AWS_REGION });
+//AWS.config.update({ region: process.env.AWS_REGION });
 
 export const call = (action, params) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
